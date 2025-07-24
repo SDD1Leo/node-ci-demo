308b4d34ede57eb911f5ab5cb4724a1fd5893b70
SDD1Leo <131460063+SDD1Leo@users.noreply.github.com>
2025-07-24 23:04:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD! this is with CD using render.'));
+app.get('/', (req, res) => res.send('Hello from CI/CD! this is with CD using render. and docker hub for container '));
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
