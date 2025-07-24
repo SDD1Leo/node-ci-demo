ef7bf1740a4bbe6cb884410b59c43cbfc7f44917
SDD1Leo <131460063+SDD1Leo@users.noreply.github.com>
2025-07-24 20:44:51 +0530

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,5 +2,9 @@
 const { add } = require('../src/index');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3); // ✅ This will pass
+});
+
+test('this test will fail', () => {
+  expect(add(1, 1)).toBe(3); // ❌ This will fail
 });
